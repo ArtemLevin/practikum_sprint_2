14f015553e3daf5e65819873ace2cc24333b9ba4
Arter Levin <xeltorox@yandex.ru>
2025-01-17 21:02:10 +0300

    Added some ideas to nginx.conf

--- docker_compose/simple_project/app/create_superuser.py
+++ docker_compose/simple_project/app/create_superuser.py
@@ -2,8 +2,7 @@ from django.contrib.auth.models import User
 import os
 
 username = os.getenv('DJANGO_SUPERUSER_USERNAME', 'admin')
-email = os.getenv('DJANGO_SUPERUSER_EMAIL', 'admin@example.com')
 password = os.getenv('DJANGO_SUPERUSER_PASSWORD', 'admin123')
 
 if not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(username=username, email=email, password=password)
+    User.objects.create_superuser(username=username, password=password)
